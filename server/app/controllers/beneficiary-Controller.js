60cc2ebaf4f10bc460cf68b2c33057a97dfb42b0
AviTiwari <tiwari.av@husky.neu.edu>
2020-04-07 02:00:17 -0400

    correcting the beneficiary logic

--- server/app/controllers/beneficiary-Controller.js
+++ server/app/controllers/beneficiary-Controller.js
@@ -117,15 +117,10 @@ let renderErrorResponse = (response) => {
 // get method for the finding items by passing id it will return 1 item
 exports.get = (request, response) => {
     const accountId = request.params.accountNumber;
-    console.log(accountId)
-    const total = beneficaryService.search(accountId)
 
+    const total = beneficaryService.search(accountId)
         .then(item => {
-            if (!item) {
-                return response.status(404).json({
-                    message: "account not found"
-                });
-            }
+
             response.status(200).json(item);
         })
         .catch(err => {
