9b050d1c8c6ec5f40920e8270a7c8425365b07d6
urvashi jain <jain.ur@husky.neu.edu>
2020-04-09 18:01:00 -0400

    addition for sign up flow in app.js and server.js

--- server/app/app.js
+++ server/app/app.js
@@ -4,5 +4,8 @@
 module.exports = (app) => {
     const beneficiaryModel = require('./models/beneficiary');
     const routes = require('./routes/beneficiary-routes');
+    const userModel = require('./models/user');
+    const registerRoutes = require('./routes/register-routes');
     routes(app);
+    registerRoutes(app);
 };
--- server/server.js
+++ server/server.js
@@ -2,13 +2,21 @@ let express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'), //created model loading here
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    userModel = require('./app/models/user'),
+    beneficiaryModel = require('./app/models/beneficiary'),
+    beneficiaryRoutes = require('./app/routes/beneficiary-routes'),
+    registerRoutes = require('./app/routes/register-routes');
 
 
 // mongoose instance connection url connection
-// mongoose.set('useNewUrlParser', true);
-mongoose.connect('mongodb+srv://root:root@exterminators-hvc8c.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
-
+//mongoose.set('useNewUrlParser', true);
+mongoose.connect('mongodb+srv://root:root@exterminators-hvc8c.mongodb.net/test?retryWrites=true&w=majority',
+{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 mongoose.Promise = global.Promise;
 
 
@@ -21,24 +29,16 @@ app.use(bodyParser.json());
 
 //Enabling CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    if (req.method === 'OPTIONS') {
-
-        req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-
-
-const initApp = require('./app/app');
-initApp(app);
+// const initApp = require('./app/app');
+// initApp(app);
+beneficiaryRoutes(app);
+registerRoutes(app);
 
 app.listen(port);
 console.log('Exterminator app API server started on: ' + port);
