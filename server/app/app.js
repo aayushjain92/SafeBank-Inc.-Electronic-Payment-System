fb7a3db53415c191f6d0983ecbc973637698d5f1
aayushjain92 <47468045+aayushjain92@users.noreply.github.com>
2020-04-12 00:56:08 -0400

    Renaming same name variables

--- server/app/app.js
+++ server/app/app.js
@@ -4,13 +4,14 @@
 module.exports = (app) => {
     const transactModel = require('./models/transactSchema');
     const accBalModel = require('./models/accountBalSchema');
-    const routes = require('./routes/transact-routes'); 
+    const transactRoutes = require('./routes/transact-routes'); 
     const beneficiaryModel = require('./models/beneficiary');
-    const routes = require('./routes/beneficiary-routes');
+    const beneficiaryRoutes = require('./routes/beneficiary-routes');
     const userModel = require('./models/user');
     const registerRoutes = require('./routes/register-routes');
     const loginRoutes = require('./routes/login-routes');
-    routes(app);
+    transactRoutes(app);
+    beneficiaryRoutes(app);
     registerRoutes(app);
     loginRoutes(app);
 };
