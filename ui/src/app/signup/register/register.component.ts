ad3b98284f2136453869a62e61e13bf51dcf9fe0
urvashi jain <jain.ur@husky.neu.edu>
2020-04-20 22:21:31 -0400

    code documentation on register component ts file

--- ui/src/app/signup/register/register.component.ts
+++ ui/src/app/signup/register/register.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
-import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { RegisterService } from 'src/app/services/register.service';
 
-
+// State for dropdown to select State in Address Form
 interface State {
   value: string;
   viewValue: string;
 }
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -16,82 +17,90 @@ interface State {
   providers: [RegisterService]
 })
 export class RegisterComponent implements OnInit {
+  //set to true so that user can navigate to next section after only providing details on previous form
   isLinear = true;
+
+  // form groups to bind form data
   personalFormGroup: FormGroup;
   addressFormGroup: FormGroup;
   credentialsFormGroup: FormGroup;
   ssnFormGroup: FormGroup;
-  minDate = new Date(1900, 0, 1);
-  maxDate =  new Date(new Date().setDate(new Date().getDate()-1))
 
+  //variables to disable current and future dates in date picker
+  minDate = new Date(1900, 0, 1);
+  maxDate = new Date(new Date().setDate(new Date().getDate() - 1))
+
+  //States dropdown values
   states: State[] = [
-    {value: 'AL', viewValue: 'Alabama'},
-    {value: 'AK', viewValue: 'Alaska'},
-    {value: 'AZ', viewValue: 'Arizona'},
-    {value: 'AR', viewValue: 'Arkansas'},
-    {value: 'CA', viewValue: 'California'},
-    {value: 'CO', viewValue: 'Colorado'},
-    {value: 'CT', viewValue: 'Connecticut'},
-    {value: 'DE', viewValue: 'Delaware'},
-    {value: 'FL', viewValue: 'Florida'},
-    {value: 'GA', viewValue: 'Georgia'},
-    {value: 'HI', viewValue: 'Hawaii'},
-    {value: 'ID', viewValue: 'Idaho'},
-    {value: 'IN', viewValue: 'Indiana'},
-    {value: 'IL', viewValue: 'Illinois'},
-    {value: 'IA', viewValue: 'Iowa'},
-    {value: 'KS', viewValue: 'Kansas'},
-    {value: 'KY', viewValue: 'Kentucky'},
-    {value: 'LA', viewValue: 'Louisiana'},
-    {value: 'ME', viewValue: 'Maine'},
-    {value: 'MD', viewValue: 'Maryland'},
-    {value: 'MA', viewValue: 'Massachusetts'},
-    {value: 'MI', viewValue: 'Michigan'},
-    {value: 'MN', viewValue: 'Minnisota'},
-    {value: 'MS', viewValue: 'Mississipi'},
-    {value: 'MO', viewValue: 'Missouri'},
-    {value: 'MT', viewValue: 'Montana'},
-    {value: 'NE', viewValue: 'Nebraska'},
-    {value: 'NV', viewValue: 'Nevada'},
-    {value: 'NH', viewValue: 'New Hampshire'},
-    {value: 'NJ', viewValue: 'New Jersey'},
-    {value: 'NM', viewValue: 'New Mexico'},
-    {value: 'NY', viewValue: 'New York'},
-    {value: 'NC', viewValue: 'North Carolina'},
-    {value: 'ND', viewValue: 'North Dakota'},
-    {value: 'OH', viewValue: 'Ohio'},
-    {value: 'OK', viewValue: 'Oklahama'},
-    {value: 'OR', viewValue: 'Oregon'},
-    {value: 'PA', viewValue: 'Pennsylvania'},
-    {value: 'RI', viewValue: 'Rhode Island'},
-    {value: 'SC', viewValue: 'South Carolina'},
-    {value: 'SD', viewValue: 'South Dakota'},
-    {value: 'TN', viewValue: 'Tennessee'},
-    {value: 'TX', viewValue: 'Texas'},
-    {value: 'UT', viewValue: 'Utah'},
-    {value: 'VT', viewValue: 'Vermont'},
-    {value: 'VA', viewValue: 'Virginia'},
-    {value: 'WA', viewValue: 'Washington'},
-    {value: 'WV', viewValue: 'West Virginia'},
-    {value: 'WI', viewValue: 'Wisconsin'},
-    {value: 'WY', viewValue: 'Wyoming'},
+    { value: 'AL', viewValue: 'Alabama' },
+    { value: 'AK', viewValue: 'Alaska' },
+    { value: 'AZ', viewValue: 'Arizona' },
+    { value: 'AR', viewValue: 'Arkansas' },
+    { value: 'CA', viewValue: 'California' },
+    { value: 'CO', viewValue: 'Colorado' },
+    { value: 'CT', viewValue: 'Connecticut' },
+    { value: 'DE', viewValue: 'Delaware' },
+    { value: 'FL', viewValue: 'Florida' },
+    { value: 'GA', viewValue: 'Georgia' },
+    { value: 'HI', viewValue: 'Hawaii' },
+    { value: 'ID', viewValue: 'Idaho' },
+    { value: 'IN', viewValue: 'Indiana' },
+    { value: 'IL', viewValue: 'Illinois' },
+    { value: 'IA', viewValue: 'Iowa' },
+    { value: 'KS', viewValue: 'Kansas' },
+    { value: 'KY', viewValue: 'Kentucky' },
+    { value: 'LA', viewValue: 'Louisiana' },
+    { value: 'ME', viewValue: 'Maine' },
+    { value: 'MD', viewValue: 'Maryland' },
+    { value: 'MA', viewValue: 'Massachusetts' },
+    { value: 'MI', viewValue: 'Michigan' },
+    { value: 'MN', viewValue: 'Minnisota' },
+    { value: 'MS', viewValue: 'Mississipi' },
+    { value: 'MO', viewValue: 'Missouri' },
+    { value: 'MT', viewValue: 'Montana' },
+    { value: 'NE', viewValue: 'Nebraska' },
+    { value: 'NV', viewValue: 'Nevada' },
+    { value: 'NH', viewValue: 'New Hampshire' },
+    { value: 'NJ', viewValue: 'New Jersey' },
+    { value: 'NM', viewValue: 'New Mexico' },
+    { value: 'NY', viewValue: 'New York' },
+    { value: 'NC', viewValue: 'North Carolina' },
+    { value: 'ND', viewValue: 'North Dakota' },
+    { value: 'OH', viewValue: 'Ohio' },
+    { value: 'OK', viewValue: 'Oklahama' },
+    { value: 'OR', viewValue: 'Oregon' },
+    { value: 'PA', viewValue: 'Pennsylvania' },
+    { value: 'RI', viewValue: 'Rhode Island' },
+    { value: 'SC', viewValue: 'South Carolina' },
+    { value: 'SD', viewValue: 'South Dakota' },
+    { value: 'TN', viewValue: 'Tennessee' },
+    { value: 'TX', viewValue: 'Texas' },
+    { value: 'UT', viewValue: 'Utah' },
+    { value: 'VT', viewValue: 'Vermont' },
+    { value: 'VA', viewValue: 'Virginia' },
+    { value: 'WA', viewValue: 'Washington' },
+    { value: 'WV', viewValue: 'West Virginia' },
+    { value: 'WI', viewValue: 'Wisconsin' },
+    { value: 'WY', viewValue: 'Wyoming' },
   ];
 
   constructor(private _formBuilder: FormBuilder, private route: ActivatedRoute,
-  private router: Router, private service : RegisterService) { }
+    private router: Router, private service: RegisterService) { }
 
   user: User = new User();
-  users : User[] = [];
-  userExists : boolean = false;
+  users: User[] = [];
+  userExists: boolean = false;
 
   ngOnInit(): void {
+    //Validations on Personal Details Form
     this.personalFormGroup = this._formBuilder.group({
-      firstNameCtrl : ['', Validators.required],
-      lastNameCtrl : ['', Validators.required],
-      phoneNumberCtrl : ['', [Validators.required, Validators.minLength, Validators.pattern("^[0-9]*$")]],
-      emailIDCtrl : ['', [Validators.required, Validators.email]],
+      firstNameCtrl: ['', Validators.required],
+      lastNameCtrl: ['', Validators.required],
+      phoneNumberCtrl: ['', [Validators.required, Validators.minLength, Validators.pattern("^[0-9]*$")]],
+      emailIDCtrl: ['', [Validators.required, Validators.email]],
       dobCtrl: ['', [Validators.required]]
     });
+    //Validations on address Details Form
     this.addressFormGroup = this._formBuilder.group({
       addressLine1Ctrl: ['', Validators.required],
       addressLine2Ctrl: ['', Validators.required],
@@ -99,30 +108,34 @@ export class RegisterComponent implements OnInit {
       stateCtrl: ['', Validators.required],
       zipCtrl: ['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(5), Validators.maxLength(5)]]
     });
-
+    //Validations on Credentials Form
     this.credentialsFormGroup = this._formBuilder.group({
-      password: new FormControl('', [Validators.required, 
+      password: new FormControl('', [Validators.required,
       Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}")])
     });
-
+    //Validations on SSN Form
     this.ssnFormGroup = this._formBuilder.group({
-      ssn: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")] )
+      ssn: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")])
     })
-    
+
   }
 
-  eventSelection(event){
+  eventSelection(event) {
     alert(event.value);
     this.user.state = event.value;
     console.log(this.user.state);
-   }
+  }
+
+  //below variables set to true to hide password and SSN on page load
   hide = true;
   hideSSN = true;
-  get passwordInput() { return this.credentialsFormGroup.get('password'); } 
-  get ssnInput() { return this.ssnFormGroup.get('ssn'); } 
+
+  //getting SSN and Password from User Input
+  get passwordInput() { return this.credentialsFormGroup.get('password'); }
+  get ssnInput() { return this.ssnFormGroup.get('ssn'); }
 
 
-  saveUser(): void{
+  saveUser(): void {
     //Personal info
     this.user.firstName = this.personalFormGroup.get('firstNameCtrl').value;
     this.user.lastName = this.personalFormGroup.get('lastNameCtrl').value;
@@ -142,30 +155,8 @@ export class RegisterComponent implements OnInit {
     //SSN Field
     this.user.ssn = this.ssnFormGroup.get('ssn').value;
 
-    //check for existing email
-    this.service.getUsers().subscribe((users:User[]) => {
-          this.users = users;
-        })
-    this.userExists = this.checkIfUserExists(this.user.email);
-
-    if(this.userExists){
-      this.router.navigate(['/register', {}]);
-    }else{
-      this.service.registerUser(this.user);
-      this.router.navigate(['/login', {}]);
-    }
+    //register user and navigate to login page
+    this.service.registerUser(this.user);
+    this.router.navigate(['/login', {}]);
   }
-
-  checkIfUserExists(email:String) : boolean{
-    var userExists : boolean;
-    this.users.forEach((user) => {
-      if(user.email == email){
-        userExists = true;
-      }else{
-        userExists = false;
-      }
-    });
-    return userExists;
-  }
-
 }
