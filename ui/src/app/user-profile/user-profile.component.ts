54a8063638214cbe1bbf0912bfea142e583359c4
urvashi jain <jain.ur@husky.neu.edu>
2020-04-20 22:58:08 -0400

    user profile validations

--- ui/src/app/user-profile/user-profile.component.ts
+++ ui/src/app/user-profile/user-profile.component.ts
@@ -5,6 +5,7 @@ import { Store } from '@ngrx/store';
 import * as fromAuth from './../store/reducers/login.reducer';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-user-profile',
@@ -12,20 +13,21 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
+  //Form Groups to get user input
   personalFormGroup: FormGroup;
   addressFormGroup: FormGroup;
   credentialsFormGroup: FormGroup;
   ssnFormGroup: FormGroup;
-  panelOpenState = false;
   user: User;
 
   constructor(private store: Store<fromAuth.State>, private _formBuilder: FormBuilder, 
     private service : UserService, private router: Router) {}
 
   ngOnInit() {
+    //getting logged in user from Store
     let authState;
     this.store.subscribe(val => authState = val);
+    //if user is ull navigate to login screen else continue
     if (authState.auth.status.user == null) {
       this.router.navigate(['/login']);
     } else {
@@ -33,32 +35,43 @@ export class UserProfileComponent implements OnInit {
       console.log(JSON.stringify(this.user));
     }
     
+    //Validations on Personal Details Form
     this.personalFormGroup = this._formBuilder.group({
-      firstNameCtrl : ['', Validators.required],
-      lastNameCtrl : ['', Validators.required],
-      phoneNumCtrl : ['', Validators.required ],
-      emailIDCtrl : ['', Validators.required],
-      dobCtrl : []
+      //Form Controllers
+      firstNameCtrl: ['', Validators.required],
+      lastNameCtrl: ['', Validators.required],
+      phoneNumberCtrl: ['', [Validators.required, Validators.minLength, Validators.pattern("^[0-9]*$")]],
+      emailIDCtrl: ['', [Validators.required, Validators.email]],
+      dobCtrl: ['', [Validators.required]]
     });
 
+    //Validations on address Details Form
     this.addressFormGroup = this._formBuilder.group({
+      //Form Controllers
       addressLine1Ctrl: ['', Validators.required],
       addressLine2Ctrl: ['', Validators.required],
       cityCtrl: ['', Validators.required],
       stateCtrl: ['', Validators.required],
-      zipCtrl: ['', Validators.required]
+      zipCtrl: ['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(5), Validators.maxLength(5)]]
     });
 
+    //Validations on Credentials Form
     this.credentialsFormGroup = this._formBuilder.group({
-      password: new FormControl('', [Validators.required, Validators.minLength(9), Validators.pattern("")])
+      //Form Controllers
+      password: new FormControl('', [Validators.required,
+      Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}")])
     });
 
+    //Validations on SSN Form
     this.ssnFormGroup = this._formBuilder.group({
-      ssn: new FormControl('', [Validators.required] ),
+      //Form Controllers
+      ssn: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")]),
+      accTypeCtrl : new FormControl(),
       accNumCtrl: new FormControl(),
       routingNumCtrl : new FormControl()
     })
     
+    //Patched values of logged in user to profile
     this.personalFormGroup.patchValue({
       firstNameCtrl : this.user.firstName,
       lastNameCtrl : this.user.lastName,
@@ -76,36 +89,51 @@ export class UserProfileComponent implements OnInit {
     });
 
     this.credentialsFormGroup.patchValue({
-      password: this.user.password
+      password: atob(this.user.password)
     });
 
     this.ssnFormGroup.patchValue({
       ssn: this.user.ssn,
       accNumCtrl: this.user.account.AccountNumber,
-      routingNumCtrl : this.user.account.routingNumber
+      routingNumCtrl : this.user.account.routingNumber,
+      accTypeCtrl : this.user.accountType
     });
   }
 
+  //to set visibility of SSN and Password
   hide = true;
   hideSSN = true;
+
+  //Getting user data from profile if updated
   get passwordInput() { return this.credentialsFormGroup.get('password'); } 
   get ssnInput() { return this.ssnFormGroup.get('ssn'); } 
 
   updateUser(){
     this.user = new User();
     //Personal info
-    //this.user.phoneNum = this.personalFormGroup.get('phoneNumCtrl').value;
+    //getting value of editatble field and setting to user input
+    this.user.phoneNumber = this.personalFormGroup.get('phoneNumCtrl').value;
+
+    //setting existing values to new object
     this.user.firstName = this.personalFormGroup.getRawValue().firstNameCtrl;
     this.user.lastName = this.personalFormGroup.getRawValue().lastNameCtrl;
     this.user.email = this.personalFormGroup.getRawValue().emailIDCtrl;
     this.user.dob = this.personalFormGroup.getRawValue().dobCtrl;
 
     //Address fields 
+    //getting value of editatble field and setting to user input
     this.user.addressLine1 = this.addressFormGroup.get('addressLine1Ctrl').value;
     this.user.addressLine2 = this.addressFormGroup.get('addressLine2Ctrl').value;
     this.user.city = this.addressFormGroup.get('cityCtrl').value;
     this.user.state = this.addressFormGroup.get('stateCtrl').value;
     this.user.zip = this.addressFormGroup.get('zipCtrl').value;
+    
+    //Account Details
+    //setting existing values to new object
+    this.user.ssn = this.ssnFormGroup.getRawValue().ssn;
+    this.user.account.AccountNumber = this.ssnFormGroup.getRawValue().accNumCtrl;
+    this.user.account.routingNumber = this.ssnFormGroup.getRawValue().routingNumCtrl;
+    this.user.accountType =  this.ssnFormGroup.getRawValue().accTypeCtrl;
 
     // base64 encoded password
     this.user.password = btoa(this.credentialsFormGroup.get('password').value);
@@ -116,7 +144,5 @@ export class UserProfileComponent implements OnInit {
         this.router.navigate(['/dashboard']);
       }
     );
-    
   }
-
 }
